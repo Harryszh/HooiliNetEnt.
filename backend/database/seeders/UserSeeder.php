5802b33a8fbfb52daae0d1f3bab8988e1a41bfd1
harryszh <harrys.zh12@gmail.com>
2025-07-20 13:29:11 +0200

    vbbhghjnbm

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -4,19 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $user = User::firstOrCreate(
-            ['name' => 'harry'],
-            [
-                'password' => Hash::make('123'),
-            ]
-        );
-
-        echo "✅ User '{$user->name}' wurde erstellt (ID: {$user->id})\n";
+        User::create([
+            'username' => 'hooli',
+            'password' => bcrypt('geheim')
+        ]);
     }
 }
